1c5b9c54e6936737cc833d8276a0452ead8766e9
Dávid Labanc <david.labanc@student.tuke.sk>
2019-08-06 16:25:42 +0200

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit',(event)=>{
     event.preventDefault()
     const search = document.querySelector('input')
     console.log(search.value)
-    fetch('http://localhost:3000/weather/?address='+search.value).then((res) => {
+    fetch('/weather/?address='+search.value).then((res) => {
         res.json().then((data) => {
             if(data)
                 console.log(data)
